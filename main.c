764c4effb3846917b179aaab1b21247d4d8e55cf
katedayun <58518683+katedayun@users.noreply.github.com>
2023-09-21 16:01:12 +0900

    1

--- main.c
+++ main.c
@@ -7,11 +7,11 @@ int main(int argc, char *argv[]) {
 	int input_int;
 	float input_float;
 	
-	printf("enter an integer : ");
+	printf("enter an integer :");
 	scanf("%d",&input_int);
 	
 	printf("enter a float : ");
-	scanf("%f",&input_float);
+	scanf("%d",&input_float);
 	
 	printf("integer : %d, float : %f\n", input_int, input_float);
 	
